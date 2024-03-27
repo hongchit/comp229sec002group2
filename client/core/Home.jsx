03c74a8971e6415f56be0b734a235fb83734116f
Alicia Singca <alice.singca@gmail.com>
2024-03-27 15:22:38 -0400

    Updated system name on main page

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
       />
       <CardContent>
         <Typography variant="body2" component="p">
-          Welcome to the MERN Skeleton home page.
+          Welcome to the Student Attendance Management System.
         </Typography>
       </CardContent>
     </Card>
