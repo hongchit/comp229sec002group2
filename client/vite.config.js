d86293693849b8abca44e2d2ef73a6f77e8ce239
Victor Tse <victortse70@gmail.com>
2024-04-08 00:14:50 -0400

    Modify vite config's PORT name

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-const { SERVER_PORT = 3000 } = process.env;
+const { PORT = 3000 } = process.env;
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: `http://localhost:${SERVER_PORT}`,
+        target: `http://localhost:${PORT}`,
         changeOrigin: true,
       },
       "/auth": {
-        target: `http://localhost:${SERVER_PORT}`,
+        target: `http://localhost:${PORT}`,
         changeOrigin: true,
       },
     },
