5d623b35d256e00e2ba8a11285ab7894c027ef99
Hong Chit Chan <hchan157@my.centennialcollege.ca>
2024-04-07 23:40:59 -0400

    Change SERVER_PORT to PORT to conform Heroku configuration for dynamically assigned port

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
-import 'dotenv/config'
+import "dotenv/config";
 
 const config = {
   env: process.env.NODE_ENV || "development",
-  port: process.env.SERVER_PORT || 3000,
+  port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri:
     process.env.MONGODB_URI ||
